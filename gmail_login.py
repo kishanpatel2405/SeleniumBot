17b0c9329052570e11f2879a5808ffb0b808d7a3
kishan <kishanvaghasiya3789@gmail.com>
2025-02-04 14:58:56 +0530

    formatecode

--- gmail_login.py
+++ gmail_login.py
@@ -1,17 +1,16 @@
-# %%
-# Import necessary libraries
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
 # Setup Chrome options to
-    #Disable the "enable-automation" flag.
-    #Add the "no-sandbox" argument.
-    #Add the "disable-infobars" argument.
-    #Add the "disable-dev-shm-usage" argument.
+# Disable the "enable-automation" flag.
+# Add the "no-sandbox" argument.
+# Add the "disable-infobars" argument.
+# Add the "disable-dev-shm-usage" argument.
 
 chrome_options = Options()
 chrome_options.add_argument("--disable-blink-features=AutomationControlled")
@@ -27,8 +26,9 @@ driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 driver.get('https://www.google.com')
 time.sleep(5)
 
-#navigate to login gmail
-driver.get("https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&hl=en&service=mail&flowName=GlifWebSignIn&flowEntry=AddSession")
+# navigate to login gmail
+driver.get(
+    "https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&hl=en&service=mail&flowName=GlifWebSignIn&flowEntry=AddSession")
 
 # Identify the user name text box and enter the value
 driver.find_element(By.ID, "identifierId").send_keys("parlayyarsu@gmail.com")
@@ -45,5 +45,5 @@ time.sleep(2)
 driver.find_element(By.XPATH, "//span[text()='Next']").click()
 time.sleep(2)
 
-#Uncomment below code to close the browser
-#driver.close()
+# Uncomment below code to close the browser
+# driver.close()
