d954e9f2c4232cd6e21cb9eb548d639405d47ca3
kishan <kishanvaghasiya3789@gmail.com>
2025-02-05 10:33:31 +0530

    formate

--- gmail_login.py
+++ gmail_login.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 username = "kisasnd@gmail.com"
 password = "9876534"
